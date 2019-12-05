44464caafa42ffb54b2b19f32db0e429e7a7673f
Hector <hramirez4@uh.edu>
2019-12-05 15:28:02 -0600

    success

--- AdminPortal/AdminPortal/users/views.py
+++ AdminPortal/AdminPortal/users/views.py
@@ -9,7 +9,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('/accounts/')
+            return redirect('/login/')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
